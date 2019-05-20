e4dc307397d30062cb7865309f6c4741d6d99f9e
r00 <therealrouix@gmail.com>
2019-05-20 13:38:43 -0700

    Enabled onboarding.

--- bot.py
+++ bot.py
@@ -23,7 +23,8 @@ import config
 from tokens import tokens as TOKENS
 
 # Extensions to load at runtime.
-startup_extensions = ['extensions.roles',
+startup_extensions = ['extensions.onboarding',
+                      'extensions.roles',
                       'extensions.twitch']
 
 
