5243d05dadf064d8108a20defc672c30a71d4fd2
r00 <therealrouix@gmail.com>
2019-08-19 04:43:17 -0700

    Adjust helpers for general commands.

--- extensions/general.py
+++ extensions/general.py
@@ -24,12 +24,12 @@ class General(commands.Cog):
 
     @commands.command(name='wishlist')
     async def wishlist_cmd(self, ctx):
-        '''Respond with Amazon wishlist link.'''
+        '''Amason stream wishlist.'''
         await ctx.send('Want to help move the stream forward? Check out the link https://www.amazon.com/hz/wishlist/ls/38YUIJLQQTS0F?ref_=wl_share !')
 
     @commands.command(name='socials', aliases=['twitter', 'instagram', 'insta'])
     async def social_command(self, ctx):
-        """Send social links."""
+        """Social links."""
         twitter = 'https://www.twitter.com/DarkPlagueDr'
         insta = 'https://www.instagram.com/DarkPlagueDr'
         await ctx.send(f'Follow me on Twitter: {twitter} or Instagram: '
@@ -37,19 +37,19 @@ class General(commands.Cog):
 
     @commands.command(name='address')
     async def address_command(self, ctx):
-        """Send address."""
+        """Shipping address."""
         await ctx.send('Shirts, spandex, and glitter bombs can be sent to P.O. '
                        'Box 28331, San Jose CA, 95159.')
 
     @commands.command(name='repo', aliases=['code', 'github', 'git'])
     async def repo_command(self, ctx):
-        """Send repo link."""
+        """Github repo."""
         URL = 'https://github.com/User-r00'
         await ctx.send(f'Check out my code at {URL} !')
 
     @commands.command(name='steam')
     async def steam_command(self, ctx):
-        """Send Steam link."""
+        """Steam profile."""
         URL = 'https://steamcommunity.com/profiles/76561198828992335/'
         await ctx.send(f'Check my Steam profile at {URL} ! Be sure to add me '
                        f'while you\'re there!')
