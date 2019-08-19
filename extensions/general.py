b1301889829141b67595a7da594cfa2449563911
r00 <therealrouix@gmail.com>
2019-08-19 04:38:07 -0700

    Add basic commands.

--- extensions/general.py
+++ extensions/general.py
@@ -25,7 +25,34 @@ class General(commands.Cog):
     @commands.command(name='wishlist')
     async def wishlist_cmd(self, ctx):
         '''Respond with Amazon wishlist link.'''
-        await ctx.send('https://www.amazon.com/hz/wishlist/ls/38YUIJLQQTS0F?ref_=wl_share')
+        await ctx.send('Want to help move the stream forward? Check out the link https://www.amazon.com/hz/wishlist/ls/38YUIJLQQTS0F?ref_=wl_share !')
+
+    @commands.command(name='socials', aliases=['twitter', 'instagram', 'insta'])
+    async def social_command(self, ctx):
+        """Send social links."""
+        twitter = 'https://www.twitter.com/DarkPlagueDr'
+        insta = 'https://www.instagram.com/DarkPlagueDr'
+        await ctx.send(f'Follow me on Twitter: {twitter} or Instagram: '
+                       f'{insta} .')
+
+    @commands.command(name='address')
+    async def address_command(self, ctx):
+        """Send address."""
+        await ctx.send('Shirts, spandex, and glitter bombs can be sent to P.O. '
+                       'Box 28331, San Jose CA, 95159.')
+
+    @commands.command(name='repo', aliases=['code', 'github', 'git'])
+    async def repo_command(self, ctx):
+        """Send repo link."""
+        URL = 'https://github.com/User-r00'
+        await ctx.send(f'Check out my code at {URL} !')
+
+    @commands.command(name='steam')
+    async def steam_command(self, ctx):
+        """Send Steam link."""
+        URL = 'https://steamcommunity.com/profiles/76561198828992335/'
+        await ctx.send(f'Check my Steam profile at {URL} ! Be sure to add me '
+                       f'while you\'re there!')
 
 
 def setup(bot):
