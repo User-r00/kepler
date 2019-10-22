1efd3764c4bd3d377b34e78cd2b0049b7c4eb615
Spaceboyr00 <15057809+User-r00@users.noreply.github.com>
2019-10-22 00:27:51 -0700

    Removed credentials

--- credentials/tokens.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-
-PROD = 'NTc3MDE4NTgxMDUzNzM0OTEy.XNi0Yg.q7gzfm5N6Yw8yJmB0vOsqBeqp94'
-UAT = ''
-TWITCH_CLIENT = 'kyfpwu0omqnerllt9il63mitnyzk7m'
-OMDB_KEY = '4c8dc5f3'
-
-# .r00
